3d8db5e76c937079f6cac4dc77682f4b423fb4ff
Ilyas Mohd <ilyas.m@lsz.ads.valuelabs.net>
2018-11-22 23:04:07 +0530

    winforms-sql connection example

---
+++ Winforms-SqlConnection/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace Winforms_SqlConnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnSP = new System.Windows.Forms.Button();
+            this.dgvCustomers = new System.Windows.Forms.DataGridView();
+            this.btnScalarExample = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgvDirectSource = new System.Windows.Forms.DataGridView();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.testDB2DataSet = new Winforms_SqlConnection.TestDB2DataSet();
+            this.customersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.customersTableAdapter = new Winforms_SqlConnection.TestDB2DataSetTableAdapters.CustomersTableAdapter();
+            this.customerIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.customerNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.custAddressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvAllTables = new System.Windows.Forms.DataGridView();
+            this.btnCustomersRead = new System.Windows.Forms.Button();
+            this.orders = new System.Windows.Forms.Button();
+            this.btnTestRead = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDirectSource)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.testDB2DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customersBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAllTables)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(92, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(147, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "read";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(42, 23);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(39, 76);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(42, 135);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(100, 20);
+            this.txtAddress.TabIndex = 5;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(42, 182);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(100, 23);
+            this.btnInsert.TabIndex = 6;
+            this.btnInsert.Text = "Insert Details";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnSP
+            // 
+            this.btnSP.Location = new System.Drawing.Point(30, 192);
+            this.btnSP.Name = "btnSP";
+            this.btnSP.Size = new System.Drawing.Size(100, 23);
+            this.btnSP.TabIndex = 7;
+            this.btnSP.Text = "Execute SP";
+            this.btnSP.UseVisualStyleBackColor = true;
+            this.btnSP.Click += new System.EventHandler(this.btnSP_Click);
+            // 
+            // dgvCustomers
+            // 
+            this.dgvCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCustomers.Location = new System.Drawing.Point(18, 14);
+            this.dgvCustomers.Name = "dgvCustomers";
+            this.dgvCustomers.Size = new System.Drawing.Size(307, 150);
+            this.dgvCustomers.TabIndex = 8;
+            // 
+            // btnScalarExample
+            // 
+            this.btnScalarExample.Location = new System.Drawing.Point(218, 204);
+            this.btnScalarExample.Name = "btnScalarExample";
+            this.btnScalarExample.Size = new System.Drawing.Size(100, 23);
+            this.btnScalarExample.TabIndex = 9;
+            this.btnScalarExample.Text = "ScalarExample";
+            this.btnScalarExample.UseVisualStyleBackColor = true;
+            this.btnScalarExample.Click += new System.EventHandler(this.btnScalarExample_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtID);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.txtAddress);
+            this.groupBox1.Controls.Add(this.btnInsert);
+            this.groupBox1.Location = new System.Drawing.Point(12, 40);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 228);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgvCustomers);
+            this.groupBox2.Controls.Add(this.btnSP);
+            this.groupBox2.Location = new System.Drawing.Point(324, 7);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(345, 221);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // dgvDirectSource
+            // 
+            this.dgvDirectSource.AutoGenerateColumns = false;
+            this.dgvDirectSource.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDirectSource.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.customerIDDataGridViewTextBoxColumn,
+            this.customerNameDataGridViewTextBoxColumn,
+            this.custAddressDataGridViewTextBoxColumn});
+            this.dgvDirectSource.DataSource = this.customersBindingSource;
+            this.dgvDirectSource.Location = new System.Drawing.Point(29, 21);
+            this.dgvDirectSource.Name = "dgvDirectSource";
+            this.dgvDirectSource.Size = new System.Drawing.Size(240, 150);
+            this.dgvDirectSource.TabIndex = 12;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dgvDirectSource);
+            this.groupBox3.Location = new System.Drawing.Point(675, 14);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(309, 221);
+            this.groupBox3.TabIndex = 13;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
+            // testDB2DataSet
+            // 
+            this.testDB2DataSet.DataSetName = "TestDB2DataSet";
+            this.testDB2DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // customersBindingSource
+            // 
+            this.customersBindingSource.DataMember = "Customers";
+            this.customersBindingSource.DataSource = this.testDB2DataSet;
+            // 
+            // customersTableAdapter
+            // 
+            this.customersTableAdapter.ClearBeforeFill = true;
+            // 
+            // customerIDDataGridViewTextBoxColumn
+            // 
+            this.customerIDDataGridViewTextBoxColumn.DataPropertyName = "CustomerID";
+            this.customerIDDataGridViewTextBoxColumn.HeaderText = "CustomerID";
+            this.customerIDDataGridViewTextBoxColumn.Name = "customerIDDataGridViewTextBoxColumn";
+            // 
+            // customerNameDataGridViewTextBoxColumn
+            // 
+            this.customerNameDataGridViewTextBoxColumn.DataPropertyName = "CustomerName";
+            this.customerNameDataGridViewTextBoxColumn.HeaderText = "CustomerName";
+            this.customerNameDataGridViewTextBoxColumn.Name = "customerNameDataGridViewTextBoxColumn";
+            // 
+            // custAddressDataGridViewTextBoxColumn
+            // 
+            this.custAddressDataGridViewTextBoxColumn.DataPropertyName = "CustAddress";
+            this.custAddressDataGridViewTextBoxColumn.HeaderText = "CustAddress";
+            this.custAddressDataGridViewTextBoxColumn.Name = "custAddressDataGridViewTextBoxColumn";
+            // 
+            // dgvAllTables
+            // 
+            this.dgvAllTables.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAllTables.Location = new System.Drawing.Point(342, 235);
+            this.dgvAllTables.Name = "dgvAllTables";
+            this.dgvAllTables.Size = new System.Drawing.Size(390, 213);
+            this.dgvAllTables.TabIndex = 14;
+            // 
+            // btnCustomersRead
+            // 
+            this.btnCustomersRead.Location = new System.Drawing.Point(748, 265);
+            this.btnCustomersRead.Name = "btnCustomersRead";
+            this.btnCustomersRead.Size = new System.Drawing.Size(100, 23);
+            this.btnCustomersRead.TabIndex = 15;
+            this.btnCustomersRead.Text = "Customers";
+            this.btnCustomersRead.UseVisualStyleBackColor = true;
+            this.btnCustomersRead.Click += new System.EventHandler(this.btnAllTables_Click);
+            // 
+            // orders
+            // 
+            this.orders.Location = new System.Drawing.Point(748, 311);
+            this.orders.Name = "orders";
+            this.orders.Size = new System.Drawing.Size(100, 23);
+            this.orders.TabIndex = 16;
+            this.orders.Text = "btnOrdersRead";
+            this.orders.UseVisualStyleBackColor = true;
+            this.orders.Click += new System.EventHandler(this.orders_Click);
+            // 
+            // btnTestRead
+            // 
+            this.btnTestRead.Location = new System.Drawing.Point(748, 361);
+            this.btnTestRead.Name = "btnTestRead";
+            this.btnTestRead.Size = new System.Drawing.Size(100, 23);
+            this.btnTestRead.TabIndex = 17;
+            this.btnTestRead.Text = "TestTable Read";
+            this.btnTestRead.UseVisualStyleBackColor = true;
+            this.btnTestRead.Click += new System.EventHandler(this.btnTestRead_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(996, 460);
+            this.Controls.Add(this.btnTestRead);
+            this.Controls.Add(this.orders);
+            this.Controls.Add(this.btnCustomersRead);
+            this.Controls.Add(this.dgvAllTables);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnScalarExample);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDirectSource)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.testDB2DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customersBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAllTables)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnSP;
+        private System.Windows.Forms.DataGridView dgvCustomers;
+        private System.Windows.Forms.Button btnScalarExample;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgvDirectSource;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private TestDB2DataSet testDB2DataSet;
+        private System.Windows.Forms.BindingSource customersBindingSource;
+        private TestDB2DataSetTableAdapters.CustomersTableAdapter customersTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn customerIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn customerNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn custAddressDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridView dgvAllTables;
+        private System.Windows.Forms.Button btnCustomersRead;
+        private System.Windows.Forms.Button orders;
+        private System.Windows.Forms.Button btnTestRead;
+    }
+}
+
---
+++ Winforms-SqlConnection/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+namespace Winforms_SqlConnection
+{
+    public partial class Form1 : Form
+    {
+        private DataSet dsAllTables;
+        //https://www.guru99.com/c-sharp-access-database.html
+        //https://docs.microsoft.com/en-us/biztalk/adapters-and-accelerators/adapter-sql/executenonquery-executereader-and-executescalar-using-the-sql-adapter
+        //https://docs.microsoft.com/en-us/dotnet/api/system.data.dataset?redirectedfrom=MSDN&view=netframework-4.7.2
+        //https://docs.microsoft.com/en-us/dotnet/framework/data/adonet/sql/sqltypes-and-the-dataset
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.AppSettings["sqlCon"]);
+
+            SqlConnection sqlConnection = new SqlConnection("Data Source=E15510S1L;Initial Catalog=TestDB2;Integrated Security=SSPI;");
+            SqlCommand command = new SqlCommand("SELECT top 2 [CustomerID],[CustomerName] FROM [dbo].[Customers]", sqlConnection);
+            command.CommandType = CommandType.Text;
+
+            //SqlConnectionStringBuilder sConnB = new SqlConnectionStringBuilder();
+            //sConnB.DataSource = ""; sConnB.InitialCatalog = ""; sConnB.UserID = ""; sConnB.Password = "";
+
+            //SqlConnection sqlConnection123 = new SqlConnection(sConnB.ConnectionString);
+
+            sqlConnection.Open();
+            SqlDataReader sqlDataReader = command.ExecuteReader();
+
+            while (sqlDataReader.Read())
+            {
+                label1.Text = sqlDataReader.GetInt32(0).ToString();
+                //label1.Text = "100";
+                label2.Text = sqlDataReader.GetString(1);
+            }
+
+            sqlConnection.Close();
+        }
+
+        public void InsertCustomer()
+        {
+            SqlConnection sqlConnection = new SqlConnection("Data Source=E15510S1L;Initial Catalog=TestDB2;Integrated Security=SSPI;");
+            //SqlCommand command = new SqlCommand(" insert into Customers values ( " + txtID.Text + ",'" + txtName.Text + "', '" + txtAddress.Text + "') ", sqlConnection);
+            SqlCommand command = new SqlCommand(" insert into Customers values (@ID, @Name, @Address) ", sqlConnection);
+            command.CommandType = CommandType.Text;
+            command.Parameters.AddWithValue("@ID", txtID.Text);
+            command.Parameters.AddWithValue("@Name", txtName.Text);
+            command.Parameters.AddWithValue("@Address", txtAddress.Text);
+
+
+            sqlConnection.Open();
+            int resul = command.ExecuteNonQuery();
+            sqlConnection.Close();
+
+            if (resul == 0)
+            {
+                MessageBox.Show("not inserted");
+            }
+            else
+            {
+                MessageBox.Show("inserted succesfully");
+
+            }
+        }
+
+        public void ScalarExample()
+        {
+            SqlConnection sqlConnection = new SqlConnection("Data Source=E15510S1L;Initial Catalog=TestDB2;Integrated Security=SSPI;");
+            SqlCommand command = new SqlCommand("select count(*) from customers", sqlConnection);
+            command.CommandType = CommandType.Text;
+
+            sqlConnection.Open();
+            object i = command.ExecuteScalar();
+            sqlConnection.Close();
+            MessageBox.Show("the no of rows are: " + Convert.ToInt32(i).ToString());
+
+        }
+
+        public void StoredProcedureExample()
+        {
+            SqlConnection sqlConnection = new SqlConnection("Data Source=E15510S1L;Initial Catalog=TestDB2;Integrated Security=SSPI;");
+            SqlCommand command = new SqlCommand("NoofCustomers", sqlConnection);
+            command.CommandType = CommandType.StoredProcedure;
+
+            //Old
+            //sqlConnection.Open();
+            //object i = command.ExecuteScalar();
+            //SqlDataReader sqlDataReader = command.ExecuteReader();
+            //DataTable dt = new DataTable();
+
+            //dt.Columns.Add("CustomerID", typeof(Int32));
+            //dt.Columns.Add("CustomerName", typeof(string));
+            //dt.Columns.Add("CustAddress", typeof(string));
+
+            //while (sqlDataReader.Read())
+            //{
+            //    dt.Rows.Add(Convert.ToInt32(sqlDataReader["CustomerId"]), sqlDataReader.GetString(1), sqlDataReader.GetString(2));
+            //}
+
+            //dgvCustomers.DataSource = dt;
+            //sqlConnection.Close();
+
+
+            //New
+            SqlDataAdapter sda = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dgvCustomers.DataSource = dt;
+
+
+            //MessageBox.Show(Convert.ToInt32(i).ToString());
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            InsertCustomer();
+        }
+
+        private void btnSP_Click(object sender, EventArgs e)
+        {
+            StoredProcedureExample();
+        }
+
+        private void btnScalarExample_Click(object sender, EventArgs e)
+        {
+            ScalarExample();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'testDB2DataSet.Customers' table. You can move, or remove it, as needed.
+            this.customersTableAdapter.Fill(this.testDB2DataSet.Customers);
+            DataSetExample();
+        }
+
+        public void DataSetExample()
+        {
+            using (SqlConnection sqlConnection = new SqlConnection("Data Source=E15510S1L;Initial Catalog=TestDB2;Integrated Security=SSPI;"))
+            {
+                SqlCommand command = new SqlCommand("select * from customers", sqlConnection);
+                command.CommandType = CommandType.Text;
+
+                SqlCommand command2 = new SqlCommand("select * from Orders", sqlConnection);
+                command2.CommandType = CommandType.Text;
+
+                SqlCommand command3 = new SqlCommand("select * from testTable", sqlConnection);
+                command3.CommandType = CommandType.Text;
+
+                DataSet ds = new DataSet("multipleTalbes");
+
+                ds.Tables.Add("Customers"); ds.Tables.Add("Orders"); ds.Tables.Add("testTable");
+
+                SqlDataAdapter da = new SqlDataAdapter(command);
+                da.Fill(ds, "Customers");
+
+
+                SqlDataAdapter da2 = new SqlDataAdapter(command2);
+                da2.Fill(ds,"Orders");
+
+                SqlDataAdapter da3 = new SqlDataAdapter(command3);
+                da3.Fill(ds, "testTable");
+
+                //dgvAllTables.DataSource = ds;
+                dsAllTables = ds;
+            }
+        }
+
+        private void btnAllTables_Click(object sender, EventArgs e)
+        {
+            dgvAllTables.DataSource = dsAllTables.Tables["Customers"];
+        }
+
+        private void orders_Click(object sender, EventArgs e)
+        {
+            dgvAllTables.DataSource = dsAllTables.Tables["Orders"];
+        }
+
+        private void btnTestRead_Click(object sender, EventArgs e)
+        {
+            dgvAllTables.DataSource = dsAllTables.Tables["testTable"];
+        }
+    }
+
+}
+
+/*
+SqlConnection conn = new SqlConnection();
+conn.ConnectionString =
+"Data Source=ServerName;" +
+"Initial Catalog=DataBaseName;" +
+"User id=UserName;" +
+"Password=Secret;";
+conn.Open();
+// .NET DataProvider -- Trusted Connection
+
+SqlConnection conn = new SqlConnection();
+conn.ConnectionString =
+"Data Source=ServerName;" +
+"Initial Catalog=DataBaseName;" +
+"Integrated Security=SSPI;";
+conn.Open();*/
+
+//SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True;User Instance=True");
+//SqlCommand cmd = new SqlCommand("select * from tbl_data", con);
+//con.Open();  
+//                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+//DataTable dt = new DataTable();
+//sda.Fill(dt);  
+//                con.Close();  
+//                dataGridView1.DataSource=dt; 
+
