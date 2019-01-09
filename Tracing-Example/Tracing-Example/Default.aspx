30c001cb8bac936015712080b5f666bde9368062
imohammed <imohammed@webmd.net>
2019-01-09 16:38:58 +0530

    code changes

---
+++ Tracing-Example/Tracing-Example/Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Tracing_Example.Default"   Trace="true"  TraceMode="SortByTime"%>
+
+<%--AutoEventWireup="false" --%>
+
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <p>My paragraph is here...the details are blow</p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Tracing-Example/Tracing-Example/Default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Tracing_Example
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        //https://odetocode.com/articles/242.aspx
+        // https://www.c-sharpcorner.com/UploadFile/225740/introduction-to-tracing/
+        //https://www.codeproject.com/Articles/82290/%2FArticles%2F82290%2FStep-by-Step-Guide-to-Trace-the-ASP-NET-Applicatio
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Trace.Write("my trace entry");
+        }
+    }
+}
